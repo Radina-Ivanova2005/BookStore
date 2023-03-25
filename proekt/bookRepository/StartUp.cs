8bfb45848d60d4111e0a07decac28292d3110c19
hira-eth <aleksandrastoyanova0522@gmail.om>
2023-03-25 16:56:57 +0200

    Changed StartUp

--- proekt/bookRepository/StartUp.cs
+++ proekt/bookRepository/StartUp.cs
@@ -6,8 +6,10 @@ public class StartUp
 {
     private static void Main(string[] args)
     {
-        //AddData addData = new AddData();
+        
 
-       Display display = new Display();
+        AddData addData = new AddData();
+
+       //Display display = new Display();
     }
 }
